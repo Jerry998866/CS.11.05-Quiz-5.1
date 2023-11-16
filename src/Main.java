4777a6febc7d1f050d70dd30a1c9c8932f23a710
Jerry998866 <jerryh2026@student.cis.edu.hk>
2023-11-16 12:32:43 +0800

    Finished Code

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
+        int [] combinedArray = new int[array1.length+array2.length];
+        for (int i = 0; i<array1.length; i++){
+            combinedArray [i] = array1[i];
+        }
+        for (int i = 0; i<array2.length; i++){
+            combinedArray [i+array1.length] = array2[i];
+        }
+        return combinedArray;
 
     }
 
@@ -21,9 +27,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int [] zipArray = new int[array1.length+array2.length];
+        int j =0;
+        for (int i = 0; i<array1.length*2; i+=2) {
+            zipArray[i] = array1[j];
+            zipArray[i + 1] = array2[j];
+            j++;
+        }
+        return zipArray;
     }
 
     /**
@@ -34,9 +45,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int [] productArray = new int[array1.length];
+        for (int i = 0; i<array1.length; i++){
+            productArray[i] = array1[i]*array2[i];
+        }
+        return productArray;
     }
 
     /**
@@ -56,15 +69,24 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
+        int[] captialArray = new int[words.length];
+        for (int i =0; i<words.length; i++){
+            captialArray[i] = countCapitalLetters(words[i]);
+        }
+        return captialArray;
 
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int captialCount = 0;
+        char[] charArray = word.toCharArray();
+        for (int i=0; i<word.length();i++){
+            int characterCode = charArray[i];
+            if (characterCode<=90 && characterCode>=65){
+                captialCount+=1;
+            }
+        }
+        return captialCount;
     }
 
 }
